446c31281d8ec7faed25f49544d3d4e8cfcd659f
karatasemre <emr3karatas@gmail.com>
2018-11-05 17:35:21 +0300

    first commit

---
+++ sample/src/main/java/com/yuyakaido/android/cardstackview/sample/MainActivity.java
@@ -0,0 +1,157 @@
+package com.yuyakaido.android.cardstackview.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.RewindAnimationSetting;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CardStackListener {
+
+    private CardStackLayoutManager manager;
+    private CardStackAdapter adapter;
+    private CardStackView cardStackView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupCardStackView();
+        setupButton();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                refresh();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCardDragging(Direction direction, float ratio) {
+        Log.d("CardStackView", "onCardDragging: d = " + direction.name() + ", r = " + ratio);
+    }
+
+    @Override
+    public void onCardSwiped(Direction direction) {
+        Log.d("CardStackView", "onCardSwiped: p = " + manager.getTopPosition() + ", d = " + direction);
+        if (manager.getTopPosition() == adapter.getItemCount() - 5) {
+            adapter.addSpots(createSpots());
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onCardRewound() {
+        Log.d("CardStackView", "onCardRewound: " + manager.getTopPosition());
+    }
+
+    @Override
+    public void onCardCanceled() {
+        Log.d("CardStackView", "onCardCanceled:" + manager.getTopPosition());
+    }
+
+    private void setupCardStackView() {
+        refresh();
+    }
+
+    private void setupButton() {
+        View skip = findViewById(R.id.skip_button);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                        .setDirection(Direction.Left)
+                        .setDuration(200)
+                        .setInterpolator(new AccelerateInterpolator())
+                        .build();
+                manager.setSwipeAnimationSetting(setting);
+                cardStackView.swipe();
+            }
+        });
+
+        View rewind = findViewById(R.id.rewind_button);
+        rewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RewindAnimationSetting setting = new RewindAnimationSetting.Builder()
+                        .setDirection(Direction.Bottom)
+                        .setDuration(200)
+                        .setInterpolator(new DecelerateInterpolator())
+                        .build();
+                manager.setRewindAnimationSetting(setting);
+                cardStackView.rewind();
+            }
+        });
+
+        View like = findViewById(R.id.like_button);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
+                        .setDirection(Direction.Right)
+                        .setDuration(200)
+                        .setInterpolator(new AccelerateInterpolator())
+                        .build();
+                manager.setSwipeAnimationSetting(setting);
+                cardStackView.swipe();
+            }
+        });
+    }
+
+    private void refresh() {
+        manager = new CardStackLayoutManager(getApplicationContext(), this);
+        manager.setStackFrom(StackFrom.Top);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.HORIZONTAL);
+        manager.setCanScrollHorizontal(true);
+        manager.setCanScrollVertical(true);
+        adapter = new CardStackAdapter(this, createSpots());
+        cardStackView = findViewById(R.id.card_stack_view);
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+    }
+
+    private List<Spot> createSpots() {
+        List<Spot> spots = new ArrayList<>();
+        spots.add(new Spot("Yasaka Shrine", "Kyoto", "https://source.unsplash.com/Xq1ntWruZQI/600x800"));
+        spots.add(new Spot("Fushimi Inari Shrine", "Kyoto", "https://source.unsplash.com/NYyCqdBOKwc/600x800"));
+        spots.add(new Spot("Bamboo Forest", "Kyoto", "https://source.unsplash.com/buF62ewDLcQ/600x800"));
+        spots.add(new Spot("Brooklyn Bridge", "New York", "https://source.unsplash.com/THozNzxEP3g/600x800"));
+        spots.add(new Spot("Empire State Building", "New York", "https://source.unsplash.com/USrZRcRS2Lw/600x800"));
+        spots.add(new Spot("The statue of Liberty", "New York", "https://source.unsplash.com/PeFk7fzxTdk/600x800"));
+        spots.add(new Spot("Louvre Museum", "Paris", "https://source.unsplash.com/LrMWHKqilUw/600x800"));
+        spots.add(new Spot("Eiffel Tower", "Paris", "https://source.unsplash.com/HN-5Z6AmxrM/600x800"));
+        spots.add(new Spot("Big Ben", "London", "https://source.unsplash.com/CdVAUADdqEc/600x800"));
+        spots.add(new Spot("Great Wall of China", "China", "https://source.unsplash.com/AWh9C-QjhE4/600x800"));
+        return spots;
+    }
+
+}
---
+++ sample/src/main/java/com/yuyakaido/android/cardstackview/sample/Spot.java
@@ -0,0 +1,13 @@
+package com.yuyakaido.android.cardstackview.sample;
+
+public class Spot {
+    public String name;
+    public String city;
+    public String url;
+
+    public Spot(String name, String city, String url) {
+        this.name = name;
+        this.city = city;
+        this.url = url;
+    }
+}
